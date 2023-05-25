ffb1e0d92f27207870d71dd85c631c0bac4250c0
Vodochnikov_Arthur <alfis.sharipov@gmail.com>
2023-05-25 10:17:59 +0300

    Код дописан

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+			Random random = new Random();
+            int number = random.Next(0, 101);
+            int attempts = 10;
+
+            Console.WriteLine("Угадайте число от 0 до 100. У вас 10 попыток.");
+			while (attempts > 0)
+            {
+                Console.Write("Попытка №" + (11 - attempts) + ": ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("Вы угадали число!");
+                    return;
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Загаданное число больше.");
+                }
         }
     }
 }
