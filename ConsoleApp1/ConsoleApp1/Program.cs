13c53f33f02311379b6763139b1aaa0c4669496b
Elena Oparina <lena.oparina.06@mail.ru>
2023-05-25 10:20:46 +0300

    Выполнела

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+			Random random = new Random();
+            int number = random.Next(0, 101);
+            int attempts = 10;
+
+            Console.WriteLine("Угадайте число от 0 до 100. У вас 10 попыток.");
+			            {
+                Console.Write("Попытка №" + (11 - attempts) + ": ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("Вы угадали число!");
+                    return;
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Загаданное число больше.");
+                }
+				else
+                {
+                    Console.WriteLine("Загаданное число меньше.");
+                }
+
+                attempts--;
+            }
+
+            Console.WriteLine("Вы не угадали число. Загаданное число: " + number);
         }
     }
 }
